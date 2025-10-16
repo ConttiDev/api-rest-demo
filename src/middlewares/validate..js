59926ef8665f5ad40a877789cd4c42b50110cacc
ConttiDev <conttidev@proton.me>
2025-10-16 19:45:27 -0300

    Implemented validation error handler

---
+++ src/middlewares/validate..js
@@ -0,0 +1,30 @@
+export const validate = (schemas = {}) => (request, _response, next) => {
+    try {
+        if (schemas.body) {
+            request.body = schemas.body.parse(request.body)
+        }
+
+        if (schemas.query) {
+            request.query = schemas.query.parse(request.query)
+        }
+
+        if (schemas.params) {
+            request.params = schemas.params.parse(request.params)
+        }
+
+        return next()
+    } catch (error) {
+        const issues = error?.issues?.map(item => ({
+            path: item.path,
+            message: item.message
+        }))
+
+        return next({
+            message: "Validation error",
+            status: 400,
+            code: "BAD_REQUEST",
+            detail: issues
+
+        })
+    }
+}
