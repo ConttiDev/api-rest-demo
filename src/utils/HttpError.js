eaab4c3aee41f1488b116db5c44da3fb76e73124
ConttiDev <conttidev@proton.me>
2025-10-16 19:51:31 -0300

    Implemented validation middleware and class validation

---
+++ src/utils/HttpError.js
@@ -0,0 +1,14 @@
+export class HttpError extends Error {
+    constructor(
+        message = "Unexpected Error",
+        status = 500,
+        code = "INTERNAL_ERROR",
+        details = null
+    ) {
+        super(message);
+
+        this.status = status
+        this.code = code
+        this.details = details
+    }
+}
